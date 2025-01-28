4dc083f9b46b98b5f5ff4cfa09f181b7b7872886
Kreya <crashaw@gmail.com>
2025-01-28 14:20:58 +0100

    refactor Footer component to use motion for animations and update layout structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,86 +1,81 @@
-import { PrismicText } from "@prismicio/react";
-import { PrismicNextLink } from "@prismicio/next";
+"use client";
+// framer motion
+import { motion } from "framer-motion";
+// animation
+import { fadeIn } from "@/utils/variants";
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+import { PrismicNextImage } from "@prismicio/next";
+
 import * as prismic from "@prismicio/client";
-
-import { Bounded } from "./Bounded";
-import { Heading } from "./Heading";
-import { PrismicRichText } from "./PrismicRichText";
-
-function SignUpForm({ settings }) {
+const Footer = ({ settings }) => {
   return (
-    <div className="px-4">
-      <form
-        action="/api/sign-up"
-        method="post"
-        className="grid w-full max-w-xl grid-cols-1 gap-6"
-      >
-        {prismic.isFilled.richText(settings.data.newsletterDisclaimer) && (
-          <div className="text-center tracking-tight text-slate-300">
-            <PrismicRichText
-              field={settings.data.newsletterDescription}
-              components={{
-                heading1: ({ children }) => (
-                  <Heading
-                    as="h2"
-                    size="6xl"
-                    className="mb-4 text-white last:mb-0"
+    <footer className="section bg-dark">
+      <div className="container mx-auto">
+        <motion.div
+          variants={fadeIn("up")}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.1 }}
+          className="flex flex-col text-white lg:flex-row lg:justify-between gap-x-5 gap-y-14"
+        >
+          <div>
+            <div className="text-xl uppercase font-primary tracking-[0.08em]">
+              {prismic.isFilled.image(settings.data.logo) && (
+                <>
+                  <PrismicNextImage
+                    field={settings.data.logo}
+                    alt="logo"
+                    width={130}
+                    height={70}
+                  />
+                </>
+              )}
+            </div>
+            <div className="text-xl uppercase font-primary tracking-[0.08em]">
+              Green Ink Art&Tattoo
+            </div>
+            <div className="leading-relaxed text-[#dbdbdb]">
+              Studio za izradu tetovaža
+            </div>
+            <div className="flex flex-col font-semibold gap-y-4 text-[#dbdbdb]">
+              {/* addresses */}
+              <div className="flex items-center gap-x-[10px]">
+                <div>
+                  <FaMapMarkerAlt />
+                </div>
+                <div>
+                  <a
+                    href="https://maps.app.goo.gl/UdHe3gR8tQMBKDa38"
+                    target="_blank"
                   >
-                    {children}
-                  </Heading>
-                ),
-                paragraph: ({ children }) => (
-                  <p className="mb-4 last:mb-0">{children}</p>
-                ),
-              }}
-            />
+                    Sarajevska 30b, Split, Croatia
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-center gap-x-[10px]">
+                <div>
+                  <FaPhoneAlt />
+                </div>
+                <div>
+                  <a href="tel:+385 97 757 2272">097 757 2272</a>
+                </div>
+              </div>
+              <div className="flex items-center gap-x-[10px]">
+                <div>
+                  <FaEnvelope />
+                </div>
+                <div>
+                  <a href="mailto:tattoo.greenink@gmail.com">
+                    tattoo.greenink@gmail.com
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-        )}
-        <div className="grid grid-cols-1 gap-2">
-          <div className="relative">
-            <label>
-              <span className="sr-only">Email address</span>
-              <input
-                name="email"
-                type="email"
-                placeholder="jane.doe@example.com"
-                required={true}
-                className="w-full rounded border border-slate-500 bg-slate-600 py-3 pl-3 pr-10 text-white placeholder-slate-400"
-              />
-            </label>
-            <button
-              type="submit"
-              className="absolute bottom-0 right-0 top-0 flex items-center justify-center px-3 text-2xl text-slate-400"
-            >
-              <span className="sr-only">Submit</span>
-              <span aria-hidden={true}>&rarr;</span>
-            </button>
-          </div>
-          {prismic.isFilled.richText(settings.data.newsletterDisclaimer) && (
-            <p className="text-center text-xs text-slate-400">
-              <PrismicText field={settings.data.newsletterDisclaimer} />
-            </p>
-          )}
-        </div>
-      </form>
-    </div>
-  );
-}
-
-export function Footer({ settings }) {
-  return (
-    <Bounded as="footer" className="bg-gray-800 pb-12 text-slate-300 md:pb-12">
-      <div className="grid grid-cols-1 justify-items-center gap-20 md:gap-24">
-        <SignUpForm settings={settings} />
-        <div className="mx-auto w-full max-w-3xl text-center text-xs font-semibold tracking-tight">
-          Web by{" "}
-          <PrismicNextLink
-            href="https://killerclick.com"
-            className="text-white"
-          >
-            Killer Click
-          </PrismicNextLink>
-        </div>
+        </motion.div>
       </div>
-    </Bounded>
+    </footer>
   );
-}
+};
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import { Header } from "./Header";
-import { Footer } from "./Footer";
+import Footer from "./Footer";
 
 export function Layout({ locales, navigation, settings, children }) {
   return (
