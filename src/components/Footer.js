d6c71460d60ee48a318d9bf6891cfec38a94a055
Kreya <crashaw@gmail.com>
2025-01-30 16:13:54 +0100

    instaled sendgrid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -73,6 +73,32 @@ const Footer = ({ settings }) => {
             </div>
           </div>
         </motion.div>
+        <div className="bg-dark text-[#dbdbdb] py-6 border-t border-[#2b2b2b]">
+          <div className="container mx-auto">
+            <motion.div
+              initial={{ y: 40, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{
+                type: "tween",
+                duration: 0.8,
+                delay: 0.2,
+              }}
+              viewport={{ once: false, amount: 0 }}
+              className="tracking-[0.02em] text-base"
+            >
+              &copy; 2025
+              <span className="font-semibold text-white"> Green Ink</span> | Web
+              by
+              <a
+                href="https://killerclick.com/"
+                target="_blank"
+                className="font-semibold ml-1"
+              >
+                Killer Click
+              </a>
+            </motion.div>
+          </div>
+        </div>
       </div>
     </footer>
   );
