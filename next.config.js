61811221c9aa6608a31f3dee532c5725f2daaf43
Kreya <crashaw@gmail.com>
2025-02-07 13:20:35 +0100

    added redirections

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = async () => {
     images: {
       domains: ["images.prismic.io"],
       dangerouslyAllowSVG: true,
-      contentDispositionType: 'attachment',
+      contentDispositionType: "attachment",
       contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
     redirects: async () => {
@@ -13,6 +13,21 @@ const nextConfig = async () => {
         {
           source: "/admin",
           destination: "https://greenink.prismic.io/",
+          permanent: true,
+        },
+        {
+          source: "/contact",
+          destination: "/hr/kontakt",
+          permanent: true,
+        },
+        {
+          source: "/about",
+          destination: "/hr/o-nama",
+          permanent: true,
+        },
+        {
+          source: "/work",
+          destination: "/hr/galerija",
           permanent: true,
         },
       ];
